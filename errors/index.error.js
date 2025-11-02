48c3625661c39a5a79c9b2362f2f69f1256ab10b
RoriLaLoutre <robin2.gillot2@gmail.com>
2025-11-02 18:22:38 +0100

    ajout de truc , je sais meme plus ou j'en suis

--- errors/index.error.js
+++ errors/index.error.js
@@ -1,3 +1,4 @@
 export * as GameError from "./api.error.js";
 export * as UserError from "./api.error.js";
 export * as RoleError from "./api.error.js";
+export * as ConfigError from "./api.error.js";
--- services/userGameConfigService.js
+++ services/userGameConfigService.js
@@ -1,4 +1,34 @@
+import { ConfigError } from "../errors/index.error.js";
 import { userGameConfigRepository } from "../repositories/indexRepository.js";
+
+
 export async function CreateUserGameConfig(data){
-    const gameConfig = await userGameConfigRepository.CreateUserGameConfig(data)
+    if(!data.id){
+        throw new ConfigError.BadRequestError("les données nécessaires à la création de la config ne sont pas réunis");
+    }
+    return await userGameConfigRepository.CreateUserGameConfig(data)
+}
+
+export async function getUserGameConfiById(id){
+    const userGameConfig = await userGameConfigRepository.getUserGameConfigById(id)
+    if(!userGameConfig){
+        throw new ConfigError.NotFoundError("config introuvable");  
+    }
+    return userGameConfig
+}
+
+export async function updateUserGameConfig(id, data){
+    if(!data.id){
+        throw new ConfigError.BadRequestError("les données nécessaires à la création de la config ne sont pas réunis");
+    }
+     await userGameConfigRepository.updateUserGameConfig(id, data)
+}
+
+
+export async function deleteUserGameConfig(id){
+    const gameConfig = await userGameConfigRepository.getUserGameConfigById(id)
+    if (!gameConfig){
+        throw new ConfigError.NotFoundError("la config n'existe pas");
+    }
+    return await userGameConfigRepository.deleteUserGameConfig(id)
 }
